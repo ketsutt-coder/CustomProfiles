38e6ad536fff09542b686be2a312a5871bc82865
ketsutt-coder <sfmf8fhr8m@privaterelay.appleid.com>
2026-02-23 04:01:31 -0600

    Update CommentCell.cpp

--- src/hooks/CommentCell.cpp
+++ src/hooks/CommentCell.cpp
@@ -3,120 +3,76 @@
 
 using namespace geode::prelude;
 
-template<typename Base, typename T>
-inline bool instanceof(const T* ptr) {
-	return dynamic_cast<const Base*>(ptr) != nullptr;
-}
-class $modify(CommentCell)
+// En Geode v5 es mejor usar typeinfo_cast directamente en lugar de templates personalizados
+class $modify(CustomCommentCell, CommentCell)
 {
-	struct Fields {
-		bool m_wasInProfilePage = false;
-	};
+    struct Fields {
+        bool m_wasInProfilePage = false;
+    };
 
-	void loadFromComment(GJComment * a1)
-	{
-		CommentCell::loadFromComment(a1);
+    void loadFromComment(GJComment* comment)
+    {
+        CommentCell::loadFromComment(comment);
 
-		if (a1->m_levelID == 0 || a1->m_hasLevelID == 1)
-		{
-			auto winSize = CCDirector::sharedDirector()->getWinSize();
-			auto commentBGColor = (CCSprite*)this->getChildren()->objectAtIndex(0);
-			commentBGColor->setVisible(false);
+        // Si el comentario no tiene nivel asociado (comentario de perfil)
+        if (comment->m_levelID == 0 || comment->m_hasLevelID == 1)
+        {
+            auto children = this->getChildren();
+            if (!children || children->count() == 0) return;
 
-			CCSize smallContentSize = { 890, 85 };
-			int X;
-			int Y;
-			Y = this->getPositionY() + 18;
-			X = this->getPositionX() + 170;
+            // Ocultamos el fondo original (índice 0)
+            if (auto commentBGColor = typeinfo_cast<CCSprite*>(children->objectAtIndex(0))) {
+                commentBGColor->setVisible(false);
+            }
 
-			if (this->m_height == 36)
-			{
-				auto smallCommentSquare = cocos2d::extension::CCScale9Sprite::create("square02b_001.png");
-				smallCommentSquare->setColor(ccBLACK);
-				smallCommentSquare->setOpacity(32);
-				smallCommentSquare->setScale(0.375);
-				smallCommentSquare->setContentSize(smallContentSize);
-				smallCommentSquare->setPositionX(X);
-				smallCommentSquare->setPositionY(Y);
-				smallCommentSquare->setZOrder(-1);
-				this->addChild(smallCommentSquare);
-			}
-			else
-			{
-				CCObject* pObj = nullptr;
-				CCARRAY_FOREACH(((CCLayer*)(this->getChildren()->objectAtIndex(1)))->getChildren(), pObj) {
+            CCSize smallContentSize = { 890, 85 };
+            float Y = this->getPositionY() + 18;
+            float X = this->getPositionX() + 170;
 
-					if (instanceof<cocos2d::extension::CCScale9Sprite>(pObj)) {
+            if (this->m_height == 36)
+            {
+                auto smallCommentSquare = cocos2d::extension::CCScale9Sprite::create("square02b_001.png");
+                smallCommentSquare->setColor(ccBLACK);
+                smallCommentSquare->setOpacity(32);
+                smallCommentSquare->setScale(0.375);
+                smallCommentSquare->setContentSize(smallContentSize);
+                smallCommentSquare->setPositionX(X);
+                smallCommentSquare->setPositionY(Y);
+                smallCommentSquare->setZOrder(-1);
+                this->addChild(smallCommentSquare);
+            }
+            else
+            {
+                // SOLUCIÓN AL ERROR: Usamos CCArrayExt en lugar de CCARRAY_FOREACH
+                if (children->count() > 1) {
+                    if (auto layer = typeinfo_cast<CCLayer*>(children->objectAtIndex(1))) {
+                        
+                        for (auto pObj : CCArrayExt<CCNode*>(layer->getChildren())) {
+                            if (auto scale9Sprite = typeinfo_cast<cocos2d::extension::CCScale9Sprite*>(pObj)) {
+                                scale9Sprite->setColor(ccBLACK);
+                                scale9Sprite->setOpacity(32);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-						cocos2d::extension::CCScale9Sprite* scale9Sprite = dynamic_cast<cocos2d::extension::CCScale9Sprite*>(pObj);
-						scale9Sprite->setColor(ccBLACK);
-						scale9Sprite->setOpacity(32);
-					}
+    // Unificamos el fix de dibujo para Android, macOS e iOS para evitar repetición
+#if defined(GEODE_IS_ANDROID) || defined(GEODE_IS_MACOS) || defined(GEODE_IS_IOS)
+    void draw()
+    {
+        auto scene = CCDirector::get()->getRunningScene();
 
-				}
-			}
-		}
+        // Fix para que no se vean bordes raros en transiciones de perfil
+        if (scene && scene->getChildByID("ProfilePage")) {
+            m_fields->m_wasInProfilePage = true;
+        }
 
-	}
-
-	//Clean comments for android and macOS
-#ifdef GEODE_IS_ANDROID
-
-		void draw()
-		{
-			auto scene = CCDirector::get()->getRunningScene();
-
-
-			//Border comments showing on transition fix
-			if (scene->getChildByID("ProfilePage")) {
-				this->m_fields->m_wasInProfilePage = true;
-			}
-
-
-			if (!this->m_fields->m_wasInProfilePage) {
-				CommentCell::draw();
-			}
-		}
-	
-#endif
-
-	//When macOS is sus
-#ifdef GEODE_IS_MACOS
-
-		void draw()
-		{
-			auto scene = CCDirector::get()->getRunningScene();
-
-
-			//Border comments showing on transition fix
-			if (scene->getChildByID("ProfilePage")) {
-				this->m_fields->m_wasInProfilePage = true;
-			}
-
-
-			if (!this->m_fields->m_wasInProfilePage) {
-				CommentCell::draw();
-			}
-		}
-#endif
-
-	//My first try to port one of my mods to iOS omg omg
-#ifdef GEODE_IS_IOS
-
-		void draw()
-		{
-			auto scene = CCDirector::get()->getRunningScene();
-
-
-			//Border comments showing on transition fix
-			if (scene->getChildByID("ProfilePage")) {
-				this->m_fields->m_wasInProfilePage = true;
-			}
-
-
-			if (!this->m_fields->m_wasInProfilePage) {
-				CommentCell::draw();
-			}
-		}
+        if (!m_fields->m_wasInProfilePage) {
+            CommentCell::draw();
+        }
+    }
 #endif
 };
